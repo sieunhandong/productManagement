ae2424adbe6ee50044963b3e270ad11dc5f377e0
Nguyen Van Dong <nvdong0902@gmail.com>
2025-02-10 20:01:37 +0700

    bai19-login-with-jwt

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -3,6 +3,9 @@ const dotenv = require('dotenv');
 const { default: mongoose } = require("mongoose");
 const routes = require('./routes');
 const bodyParser = require("body-parser");
+// Import Swagger
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 dotenv.config()
 
 const app = express()
@@ -11,6 +14,27 @@ const port = process.env.PORT || 3001
 // app.get('/', (req, res) => {
 //     res.send('Hello world')
 // })
+// Cấu hình Swagger
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Product Management API",
+            version: "1.0.0",
+            description: "API quản lý sản phẩm và người dùng"
+        },
+        servers: [
+            {
+                url: "http://localhost:3001"
+            }
+        ]
+    },
+    apis: ["./routes/*.js"] // Chỉ định file chứa API docs (Cần tạo Swagger trong `routes`)
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use(bodyParser.json())
 
 routes(app);
--- back-end/src/services/UserService.js
+++ back-end/src/services/UserService.js
@@ -1,4 +1,7 @@
 const User = require("../models/UserModel")
+const bcrypt = require("bcrypt")
+const { genneralAccessToken, genneralRefreshToken } = require("./JwtService")
+
 
 const createUser = (newUser) => {
     return new Promise(async (resolve, reject) => {
@@ -13,11 +16,12 @@ const createUser = (newUser) => {
                     message: 'The email is already'
                 })
             }
+            const hash = bcrypt.hashSync(password, 10)
+            //console.log('hash', hash)
             const createUser = await User.create({
                 name,
                 email,
-                password,
-                confirmPassword,
+                password: hash,
                 phone
             })
             if (createUser) {
@@ -33,6 +37,51 @@ const createUser = (newUser) => {
     })
 }
 
+const loginUser = (userLogin) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, email, password, confirmPassword, phone } = userLogin
+        try {
+            const checkUser = await User.findOne({
+                email: email
+            })
+            if (checkUser === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The user is not defined'
+                })
+            }
+            const comparePassword = bcrypt.compareSync(password, checkUser.password)
+
+            if (!comparePassword) {
+                resolve({
+                    status: 'OK',
+                    message: 'The password or user is incorrect',
+                })
+            }
+            const access_token = genneralAccessToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+
+            const refresh_token = genneralRefreshToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+
+            console.log('access_token', access_token)
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                access_token,
+                refresh_token
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
-    createUser
+    createUser,
+    loginUser
 }
