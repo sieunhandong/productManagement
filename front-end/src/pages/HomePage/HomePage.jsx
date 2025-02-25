7c7a07163a64bdaec2230224248183c8a4b99e9e
Nguyen Van Dong <nvdong0902@gmail.com>
2025-02-25 23:21:15 +0700

    bai8-create-navbar

--- front-end/src/pages/HomePage/HomePage.jsx
+++ front-end/src/pages/HomePage/HomePage.jsx
@@ -7,6 +7,7 @@ import slide2 from '../../assets/images/slide2.png';
 import slide3 from '../../assets/images/slide3.png';
 import slide4 from '../../assets/images/emyeu.png';
 import CardComponent from '../../components/CardComponent/CardComponent';
+import NavbarComponent from '../../components/NavbarComponent/NavbarComponent';
 function HomePage() {
   const arr = ['TV', 'Tu lanh', 'Lap top']
   return (
@@ -25,6 +26,7 @@ function HomePage() {
         <div style={{ marginTop: "20px", display: "flex", alignItems: "center", gap: "20px" }}>
           <CardComponent />
         </div>
+        <NavbarComponent />
       </div>
     </>
   )
