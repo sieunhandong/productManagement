4035497e15937dea1c5928c2e43017aed84b487d
phuc-cyber <nguyenhoangphucxdd@gmail.com>
2025-04-16 00:37:04 +0700

    commit-table-drawer-modal-component

---
+++ front-end/src/components/TableComponent/TableComponent.jsx
@@ -0,0 +1,32 @@
+import { Divider, Radio, Table } from 'antd'
+import React, { useState } from 'react'
+import Loading from '../LoadingComponent/Loading';
+
+const TableComponent = (props) => {
+    const { selectionType = 'checkbox', data = [], isLoading = false, columns = [] } = props
+
+    const rowSelection = {
+        onChange: (selectedRowKeys, selectedRows) => {
+            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+        },
+        getCheckboxProps: (record) => ({
+            disabled: record.name === 'Disabled User', // Column configuration not to be checked
+            name: record.name,
+        }),
+    };
+    return (
+        <Loading isLoading={isLoading}>
+            <Table
+                rowSelection={{
+                    type: selectionType,
+                    ...rowSelection
+                }}
+                columns={columns}
+                dataSource={data}
+                {...props}
+            />
+        </Loading>
+    )
+}
+
+export default TableComponent
